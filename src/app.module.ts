478bb02d0eff1d4d7c0cb7c2b36077459255b30b
ProVitSer <prokinvs@gmail.com>
2021-10-26 22:19:05 +0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '@app/config/config.provides';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './logger/logger.service';
+import { LoggerModule } from './logger/logger.module';
+import { LowdbService } from './lowdb/lowdb.service';
+import { LowdbModule } from './lowdb/lowdb.module';
+import { AmiModule } from './ami/ami.module';
+import { DatabaseModule } from './database/database.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ApiModule } from './api/api.module';
+import { SyncDataModule } from './sync-data/sync-data.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({ load: [configuration] }),
+      EventEmitterModule.forRoot(),
+      LoggerModule,
+      LowdbModule,
+      AmiModule,
+      ApiModule,
+      SyncDataModule,
+      //DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [ConfigModule],
 })
-export class AppModule {}
+export class AppModule { }
