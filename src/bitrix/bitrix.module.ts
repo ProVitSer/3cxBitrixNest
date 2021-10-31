cfbe6c1f7b09d49525dca2bc5b1ce522c19e8cb1
ProVitSer <prokinvs@gmail.com>
2021-10-31 22:49:13 +0300

    add DatabaseModule

--- src/bitrix/bitrix.module.ts
+++ src/bitrix/bitrix.module.ts
@@ -1,4 +1,5 @@
 import { ApiModule } from '@app/api/api.module';
+import { DatabaseModule } from '@app/database/database.module';
 import { LoggerModule } from '@app/logger/logger.module';
 import { LowdbModule } from '@app/lowdb/lowdb.module';
 import { Module } from '@nestjs/common';
@@ -6,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { BitrixService } from './bitrix.service';
 
 @Module({
-  imports:[ConfigModule, LoggerModule, LowdbModule, ApiModule],
+  imports:[ConfigModule, LoggerModule, LowdbModule, ApiModule, DatabaseModule],
   providers: [BitrixService],
   exports:[BitrixService]
 })
