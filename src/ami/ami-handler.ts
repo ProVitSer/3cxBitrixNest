58299eca1ca8a1f9eba8f096660fef2f9f735eeb
ProVitSer <prokinvs@gmail.com>
2021-11-06 20:37:31 +0300

    Итоговые обработки событий

--- src/ami/ami-handler.ts
+++ src/ami/ami-handler.ts
@@ -19,30 +19,32 @@ export class AmiEventsHandlers {
 
     ) {}
 
-  sleep() {
+  sleep(ms) {
     return new Promise((resolve) => {
-      setTimeout(resolve, 20000);
+      setTimeout(resolve, ms);
     });
   }
 
   @OnEvent(EventEmitterType.OutgoingCall)
   async outgoing(event: EndOutgoingCall) {
-    this.log.info(`Завершился исходящий вызов на Asterisk${event}`);
-    await this.sleep();
+    this.log.info(`Завершился исходящий вызов на Asterisk ${event}`);
+    await this.sleep(40000);
+    return await this.bitrix.sendCallInfoByOutgoingCall(event.appdata as EndOutgoingCall)
+
   }
 
   @OnEvent(EventEmitterType.IncomingCall)
   async incoming(event: EndIncomigCall) {
     this.log.info(`Завершился входящий вызов на Asterisk ${event}`);
-    await this.sleep();
+    await this.sleep(50000);
     return await this.bitrix.sendInfoByIncomingCall(event.appdata as EndIncomigCall)
   }
 
   @OnEvent(EventEmitterType.CrmCall)
   async crmCall(event: Event) {
     this.log.info(`Завершился исходящий вызов на Asterisk через CRM ${event}`);
-    await this.sleep();
-    return await this.bitrix.sendCallInfoByOutgoingCall(event.appdata as EndCrmCall)
+    await this.sleep(20000);
+    return await this.bitrix.sendCallInfoByOutgoingCRMCall(event.appdata as EndCrmCall)
   }
 
   @OnEvent(EventEmitterType.InitIncomingCall)
